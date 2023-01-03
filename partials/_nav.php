862d50ffa82f2e36f617466012b014a0411dd337
Shrutii9 <112260757+Shrutii9@users.noreply.github.com>
2023-01-03 15:45:52 +0530

    Add files via upload

---
+++ partials/_nav.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <div class="logo">
+    <!--<a class="navbar-brand" href="/mylist">TO DO LIST</a>-->
+    <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.png" alt="">
+        <span>Todo List</span>
+      </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/loginsystem/index.html">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/loginsystem/login.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/loginsystem/signup.php">Signup</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/loginsystem/ContactUs.html">About Us</a>
+      </li>
+      </ul>
+    </div>
+  </div>
+</nav>
